0ddace1526b6229bbd7128c52b3c54d25b4c63db
Stephane P. Pericat <contact@pericat.net>
2019-09-08 13:56:16 -0500

    dev server tweak

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
   resolve: {
     alias: {
-      svelte: path.resolve('node_modules', 'svelte')
+      svelte: path.resolve("node_modules", "svelte")
     },
-    extensions: ['.mjs', '.js', '.svelte'],
-    mainFields: ['svelte', 'browser', 'module', 'main']
+    extensions: [".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"]
   },
   module: {
     rules: [
       {
         test: /\.(html|svelte)$/,
         exclude: /node_modules/,
-        use: 'svelte-loader'
+        use: "svelte-loader"
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin()
-  ]
-}
+  plugins: [new HtmlWebpackPlugin()],
+  devServer: {
+    open: true
+  }
+};
