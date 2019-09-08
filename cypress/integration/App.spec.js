e229828d58277097dadd6c1205860511946e4cbd
Stephane P. Pericat <contact@pericat.net>
2019-09-08 13:04:29 -0500

    added cypress test example

---
+++ cypress/integration/App.spec.js
@@ -0,0 +1,16 @@
+import App from "../../src/App.svelte";
+import mount from "cypress-svelte-unit-test";
+
+describe("App component", () => {
+  it("shoul display a greeting", () => {
+    cy.viewport(960, 500);
+
+    mount(App, {
+      props: {
+        name: "Cypress"
+      }
+    });
+
+    cy.get("h1").should("contain", "Hello Cypress!")
+  })
+})
